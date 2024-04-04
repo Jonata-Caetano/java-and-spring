23f203f1f054bf8a71968b0521bdeff391b5c99e
Jonata Caetano <jonatacaetano88@gmail.com>
2024-04-03 23:15:28 -0300

    "Scenario counter and assert"

--- cucumber/src/test/java/LearnCucumber.java
+++ cucumber/src/test/java/LearnCucumber.java
@@ -2,6 +2,7 @@ import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class LearnCucumber {
@@ -17,4 +18,24 @@ public class LearnCucumber {
     @Então("a especificacao deve finalizar com sucesso")
     public void aEspecificacaoDeveFinalizarComSucesso() {
     }
+
+    private Integer contador = 15;
+
+    @Dado("que o valor do contador é {int}")
+    public void queOValorDoContadorE(Integer arg0) {
+        contador = arg0;
+    }
+
+    @Quando("eu incrementar em {int}")
+    public void euIncremetarEm(Integer arg1) {
+        contador += arg1;
+    }
+
+    @Então("o valor do contador sera {int}")
+    public void oValorDoContadorSera(Integer arg2) {
+        System.out.println(arg2);
+        System.out.println(contador);
+        assertEquals(arg2, contador);
+    }
+
 }
--- cucumber/src/test/java/resources/learn_cucumber.feature
+++ cucumber/src/test/java/resources/learn_cucumber.feature
@@ -4,7 +4,12 @@ Funcionalidade: Aprender Cucumber
   Eu quero aprender a utilizar o Cucumber
   Para que eu possa automatizar criterios de aceitacao
 
-Cenario: Deve executar especificacao
-  Dado que criei o arquivo corretamente
-  Quando executa-lo
-  Então a especificacao deve finalizar com sucesso
+  Cenario: Deve executar especificacao
+    Dado que criei o arquivo corretamente
+    Quando executa-lo
+    Então a especificacao deve finalizar com sucesso
+
+  Cenario: Deve incrementar contador
+    Dado que o valor do contador é 15
+    Quando eu incrementar em 5
+    Então o valor do contador sera 18
